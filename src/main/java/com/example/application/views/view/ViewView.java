238a7ec6c8cba4417940a64038f144d360b10a55
start.vaadin.com <no-reply@vaadin.com>
2025-04-23 14:18:24 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/view/ViewView.java
@@ -0,0 +1,35 @@
+package com.example.application.views.view;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("View")
+@Route("view")
+@Menu(order = 2, icon = LineAwesomeIconUrl.PENCIL_RULER_SOLID)
+public class ViewView extends Composite<VerticalLayout> {
+
+    public ViewView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("min-content");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.setWidth("100%");
+        layoutRow2.setHeight("min-content");
+        getContent().add(layoutRow);
+        getContent().add(layoutColumn2);
+        getContent().add(layoutRow2);
+    }
+}
